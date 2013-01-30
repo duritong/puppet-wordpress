a2ab4654eac09e618209a490e5001402a6405adf
mh <mh@immerda.ch>
2013-01-30 10:52:34 +0100

    installation mode should not be restricted as scripts is run as sftp user

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,12 +1,16 @@
+# basic things for automated wordpress installations
 class wordpress::base(
   $mgmt_tools_git_repo = hiera('wordpress_mgmt_tools_git_repo','git://git.immerda.ch/wordpress-cli-installer.git')
 ) {
   file{'/var/www/wordpress_tools':
-    ensure => directory,
-    owner => root, group => 0, mode => 0640;
+    ensure  => directory,
+    owner   => root,
+    group   => 0,
+    mode    => '0644';
   }
-  git::clone{"wordpress_mgmt_tools":
-    git_repo => $wordpress::base::mgmt_tools_git_repo,
-    projectroot => '/var/www/wordpress_tools/installer',
+  git::clone{'wordpress_mgmt_tools':
+    git_repo                => $mgmt_tools_git_repo,
+    projectroot             => '/var/www/wordpress_tools/installer',
+    cloneddir_restrict_mode => false;
   }
 }
