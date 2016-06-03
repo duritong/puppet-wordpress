3021017fb95109c3eab6f5953fd077a3003e4e90
mh <mh@immerda.ch>
2016-06-03 16:18:40 +0200

    on a local installation be sure to not install wp before the database

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,6 +1,6 @@
 # basic things for automated wordpress installations
 class wordpress::base(
-  $default_dbhost            = 'localhost',
+  $default_dbhost            = '127.0.0.1',
   $default_active_plugins    = [ 'si-captcha-for-wordpress',
     'disable-google-fonts' ],
   $default_installed_plugins = [ 'wp-super-cache', 'backupwordpress', ],
--- manifests/instance.pp
+++ manifests/instance.pp
@@ -92,6 +92,12 @@ PHP
         subscribe   => Exec["install_wordpress_${name}"],
         before      => Service['apache'];
     }
+    # make sure we get the local relations right
+    if $install_options['dbhost'] in ['localhost','127.0.0.1','::1'] {
+      $dbname = $install_options['dbname']
+      Mysql_database<| title == $dbname |> -> Exec["config_wordpress_${name}"]
+      Mysql_user<| title == $dbuser |> -> Exec["config_wordpress_${name}"]
+    }
 
     $installed_plugins = suffix(union($install_options['installed_plugins'],
       $wordpress::base::default_installed_plugins),"@${name}")
