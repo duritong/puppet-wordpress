3709713e1e4aeb37a965fc11d4c1f61537626530
mh <mh@immerda.ch>
2019-05-28 21:48:39 +0200

    also needs the cli

--- manifests/instance/active_plugin.pp
+++ manifests/instance/active_plugin.pp
@@ -1,5 +1,6 @@
 # activated & installed plugin
 define wordpress::instance::active_plugin(
+  $wp_cli,
   $user,
   $group,
   $path,
@@ -7,11 +8,12 @@ define wordpress::instance::active_plugin(
   $infos = split($name,'@')
   wordpress::instance::plugin{
     $name:
-      user  => $user,
-      group => $group,
-      path  => $path,
+      wp_cli => $wp_cli,
+      user   => $user,
+      group  => $group,
+      path   => $path,
   } ~> exec{"activate_plugin_${infos[0]}_wordpress_${infos[1]}":
-    command     => "wp --path=${path} --no-color plugin activate ${infos[0]}",
+    command     => "${wp_cli} plugin activate ${infos[0]}",
     refreshonly => true,
     user        => $user,
     group       => $group,
